5949ea0cee4f7d08bce817a93c7cada2703b7d10
Dreamania Code <flakeheider@gmail.com>
2026-02-26 01:31:42 +0000

    Final build and lint fixes for production

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,16 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      ".vercel/**",
+      "out/**",
+      "build/**",
+      "scripts/**",
+    ],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
